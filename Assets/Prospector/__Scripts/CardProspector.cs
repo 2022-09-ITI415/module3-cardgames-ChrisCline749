55081783112f5e498a0efd2c77187b7957bfdd8f
ChrisCline749 <46876667+ChrisCline749@users.noreply.github.com>
2022-11-03 17:42:03 -0400

    converts cards to cardProspectors

--- Assets/Prospector/__Scripts/CardProspector.cs
+++ Assets/Prospector/__Scripts/CardProspector.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardProspector : MonoBehaviour
+public enum eCardState
 {
-    public enum eCardState
-    {
-        drawpile,
-        tableau,
-        target,
-        discard
-    }
+    drawpile,
+    tableau,
+    target,
+    discard
+}
 
-    public class Prospector : Card
-    {
+public class CardProspector : Card
+{
         [Header("Set Dynamically: CardProspector")]
 
         public eCardState state = eCardState.drawpile;
         public List<CardProspector> hiddenBy = new List<CardProspector>();
         public int layoutID;
         public SlotDef slotDef;
-    }
 }
